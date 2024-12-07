3aac8edd4b8632e21594b518dac86fdd43b7535a
Alexdanbento <skollpelados@gmail.com>
2024-12-06 22:59:56 -0300

    Update vite-config.js

--- vite-config.js
+++ vite-config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // Altere esta linha
+  base: '/carioca/' // Ajustado para o nome do seu repositório
 })
